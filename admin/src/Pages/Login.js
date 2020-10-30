67eb5451cf763b5954408cd2a415e67f84b87c2a
liuhuan086 <liuhuan086>
2020-10-30 22:38:04 +0800

    登陆页面样式

--- admin/src/Pages/Login.js
+++ admin/src/Pages/Login.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css'
+import {Card, Input, Icon, Button, Spin} from "antd";
+import '../static/css/Login.css'
 
 function Login() {
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const checkLogin = () => {
+        setIsLoading(true)
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 1000)
+    }
+
     return (
-        <div>
-            登陆页面
+        <div className='login-div'>
+            <Spin tip='Loading...' spinning={isLoading}>
+                <Card title="Huan's Blog System" boardered={true} style={{width: 400}}>
+                    <Input
+                        id='userName'
+                        size='large'
+                        placeholder='Enter your username'
+                        prefix={<Icon type='user'
+                                      style={{color: 'rgba(0,0,0,.25)'}}/>}
+                        onChange={(e) => {
+                            setUserName(e.target.value)
+                        }}
+                    />
+                    
+                    <br/><br/>
+
+                    <Input.Password
+                        id='password'
+                        size='large'
+                        placeholder='Enter your password'
+                        prefix={<Icon type='key'
+                                      style={{color: 'rgba(0,0,0,.25)'}}/>}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                    />
+                    <br/><br/>
+
+                    <Button type="primary" size='large' block onClick={checkLogin}>Login</Button>
+
+                </Card>
+            </Spin>
+
         </div>
     )
 }
