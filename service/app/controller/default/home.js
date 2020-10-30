6f688a6681780fb7b805911cd49aff4c99fe5b84
liuhuan086 <liuhuan086>
2020-10-30 17:18:44 +0800

    根据文章类型获取文章列表

--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -5,9 +5,9 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
 
     async index() {
-        let result = await this.app.mysql.get("blog_content",{})
+        let result = await this.app.mysql.get("blog_content", {})
         console.log(result)
-        this.ctx.body=result
+        this.ctx.body = result
 
         // this.ctx.body = 'api hi'
     }
@@ -49,6 +49,22 @@ class HomeController extends Controller {
         this.ctx.body = {data: result}
     }
 
+    async getListById() {
+
+        let id = this.ctx.params.id
+
+        let sql = 'SELECT article.id as id,'+
+            'article.title as title,'+
+            'article.introduce as introduce,'+
+            "FROM_UNIXTIME(article.add_time,'%Y-%m-%d %H:%i:%s' ) as add_time,"+
+            'article.view_count as view_count ,'+
+            'type.typeName as typeName '+
+            'FROM article LEFT JOIN type ON article.type_id = type.Id '+
+            'WHERE type_id='+id
+
+        const result = await this.app.mysql.query(sql)
+        this.ctx.body = {data: result}
+    }
 }
 
 module.exports = HomeController;
