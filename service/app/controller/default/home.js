be8963b259bf6d7b3ca8872209e84ee4d1c88c12
liuhuan086 <liuhuan086>
2020-11-02 09:28:23 +0800

    修复一些bug

--- service/app/controller/default/home.js
+++ service/app/controller/default/home.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
 
     async index() {
-        this.ctx.body='api hi'
+        this.ctx.body = 'api hi'
     }
 
     async getArticleList() {
@@ -49,14 +49,14 @@ class HomeController extends Controller {
 
         let id = this.ctx.params.id
 
-        let sql = 'SELECT article.id as id,'+
-            'article.title as title,'+
-            'article.introduce as introduce,'+
-            "FROM_UNIXTIME(article.add_time,'%Y-%m-%d %H:%i:%s' ) as add_time,"+
-            'article.view_count as view_count ,'+
-            'type.typeName as typeName '+
-            'FROM article LEFT JOIN type ON article.type_id = type.Id '+
-            'WHERE type_id='+id
+        let sql = 'SELECT article.id as id,' +
+            'article.title as title,' +
+            'article.introduce as introduce,' +
+            "FROM_UNIXTIME(article.add_time,'%Y-%m-%d %H:%i:%s' ) as add_time," +
+            'article.view_count as view_count ,' +
+            'type.typeName as typeName ' +
+            'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+            'WHERE type_id=' + id
 
         const result = await this.app.mysql.query(sql)
         this.ctx.body = {data: result}
